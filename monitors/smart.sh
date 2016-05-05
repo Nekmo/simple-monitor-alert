7dc27216952007d972c7623dbf40723c2b8b4bb4
Nekmo <contacto@nekmo.com>
2016-05-05 17:09:31 +0200

    Hddtemp.

--- monitors/smart.sh
+++ monitors/smart.sh
@@ -9,8 +9,8 @@ echo "X-Run-Every-Seconds: 43200"
 for disk in `lsblk -d -o name | tail -n +2`; do
     status=`sudo smartctl -H /dev/$disk | tail -n 2 | head -n +1 | awk '{print $NF}'`
     if [[ $status != 'summary' && $? != 1 ]]; then
-        echo "smart_status.name = 'SMART test result on /dev/$disk'"
-        echo "smart_status.expected = 'PASSED'"
-        echo "smart_status.value = '$status'"
+        echo "smart_status($disk).name = 'SMART test result on /dev/$disk'"
+        echo "smart_status($disk).expected = 'PASSED'"
+        echo "smart_status($disk).value = '$status'"
     fi
 done
