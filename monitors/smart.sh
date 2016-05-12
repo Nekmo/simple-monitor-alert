32bb3869b34b1c762120ecb48b27955412fe0565
Nekmo <contacto@nekmo.com>
2016-05-13 01:14:29 +0200

    ISSUE-simple-monitor-alert-10 Incorrect Smart parsing

--- monitors/smart.sh
+++ monitors/smart.sh
@@ -7,8 +7,8 @@
 echo "X-Run-Every-Seconds: 43200"
 
 for disk in `lsblk -d -o name | tail -n +2`; do
-    status=`sudo smartctl -H /dev/$disk | tail -n 2 | head -n +1 | awk '{print $NF}'`
-    if [[ $status != 'summary' && $? != 1 ]]; then
+    status=`sudo smartctl -H /dev/$disk | grep -e 'overall-health' | awk '{print $NF}'`
+    if [[ $? != 1 && $status ]]; then
         echo "smart_status.name = 'SMART test result on /dev/$disk'"
         echo "smart_status.expected = 'PASSED'"
         echo "smart_status.value = '$status'"
