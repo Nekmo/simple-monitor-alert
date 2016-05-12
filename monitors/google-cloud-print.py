276cb3d81876d4eed8bdf2a7717fd18fbbb9d8c1
Nekmo <contacto@nekmo.com>
2016-05-12 05:01:58 +0200

    Auth pin code.

--- monitors/google-cloud-print.py
+++ monitors/google-cloud-print.py
@@ -5,11 +5,16 @@ import sys
 import webbrowser
 import json
 
+import requests
+import time
+
 from simple_monitor_alert.sma import get_var_directory
 
-REGISTER_NEW_PROJECT_URL = 'https://console.cloud.google.com/iam-admin/projects'
-
+GRANT_TYPE = 'http://oauth.net/grant_type/device/1.0'
+SCOPE = 'https://www.googleapis.com/auth/cloudprint'
+REGISTER_NEW_PROJECT_URL = 'https://console.developers.google.com/iam-admin/projects'
 OAUTH_DEVICE_CODE_URL = 'https://accounts.google.com/o/oauth2/device/code'
+OAUTH_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token'
 
 var_directory = get_var_directory()
 auth_file = os.path.join(var_directory, 'google-cloud-print.json')
@@ -24,15 +29,45 @@ def register():
     webbrowser.open(REGISTER_NEW_PROJECT_URL)
     print('2. Create a new project.')
     print('3. Go to "credentials" and create a new Oauth ID Client".')
+    print('Credentials url: https://console.developers.google.com/apis/credentials')
     print('4. Select "other" in application type. You may need to create an authentication screen before.')
     print('5. Now you must have: client ID and client Secret.')
     print('Client ID example: 137998618127-d0p9bh4k48gugfh0l50u59okri7bd2xr.apps.googleusercontent.com')
     print('Client Secret example: 1kuJGYaCEWpGIcqTP7gNHd5c')
+    time.sleep(2)
     client_id = input('Insert client ID: ')
     client_secret = getpass.getpass('Insert Client Secret (hidden): ')
-    json.dump({'client_id': client_id, 'client_secret': client_secret}, open(auth_file, 'w'))
+    data = {}
+    while True:
+        r = requests.post(OAUTH_DEVICE_CODE_URL, {'scope': SCOPE, 'client_id': client_id})
+        data = r.json()
+        print('Go to {verification_url} and insert: {user_code}'.format(**data))
+        print('INSERT CODE ==========> {} <========== INSERT CODE'.format(data['user_code']))
+        webbrowser.open(data['verification_url'])
+        time.sleep(2)
+        print("[!!] Press enter when ready")
+        input('[Press enter]')
+        r = requests.post(OAUTH_TOKEN_URL, {'client_secret': client_secret, 'code': data['device_code'],
+                                            'client_id': client_id, 'grant_type': GRANT_TYPE})
+        data = r.json()
+        if data.get('error'):
+            print('Error: {error}'.format(**data))
+            print('Sorry, It will start again.')
+        else:
+            print('Great!')
+            break
+    data.update({'client_id': client_id, 'client_secret': client_secret})
+    json.dump(data, open(auth_file, 'w'))
     print('Created {}'.format(auth_file))
 
 
+def auth(file=None):
+    file = file or auth_file
+    if not file:
+        print('You need execute "register". Look at the instructions.')
+        raise SystemExit
+    auth = json.load(open(auth_file))
+
 if __name__ == '__main__':
-    register()
+    if len(sys.argv) > 1 and sys.argv[1] == 'register':
+        register()
