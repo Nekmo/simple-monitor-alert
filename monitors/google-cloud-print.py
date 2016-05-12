baf62f668dc87e31800a7dbe04c27b6e1ec47947
Nekmo <contacto@nekmo.com>
2016-05-12 05:52:35 +0200

    get prints function.

--- monitors/google-cloud-print.py
+++ monitors/google-cloud-print.py
@@ -15,12 +15,26 @@ SCOPE = 'https://www.googleapis.com/auth/cloudprint'
 REGISTER_NEW_PROJECT_URL = 'https://console.developers.google.com/iam-admin/projects'
 OAUTH_DEVICE_CODE_URL = 'https://accounts.google.com/o/oauth2/device/code'
 OAUTH_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token'
+SEARCH_PRINTS = 'https://www.google.com/cloudprint/search'
 
 var_directory = get_var_directory()
 auth_file = os.path.join(var_directory, 'google-cloud-print.json')
 
 if sys.version_info < (3, 0):
     input = raw_input
+
+
+class Auth(requests.auth.AuthBase):
+    def __init__(self, file=None):
+        self.file = file or auth_file
+        if not self.file:
+            print('You need execute "register". Look at the instructions.')
+            raise SystemExit
+        self.data = json.load(open(self.file))
+
+    def __call__(self, r):
+        r.headers['Authorization'] = '{token_type} {access_token}'.format(**self.data)
+        return r
 
 
 def register():
@@ -61,13 +75,20 @@ def register():
     print('Created {}'.format(auth_file))
 
 
-def auth(file=None):
-    file = file or auth_file
-    if not file:
-        print('You need execute "register". Look at the instructions.')
-        raise SystemExit
-    auth = json.load(open(auth_file))
+def get_prints(auth=None):
+    auth = auth or Auth()
+    r = requests.get(SEARCH_PRINTS, auth=auth)
+    data = r.json()
+    for printer in data['printers']:
+        if printer['name'] in ['Save to Google Docs']:
+            continue
+        print('printer_status({id}).name = "{displayName}"'.format(**printer))
+        print('printer_status({id}).expected = "ONLINE"'.format(**printer))
+        print('printer_status({id}).value = "{connectionStatus}"'.format(**printer))
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'register':
         register()
+    else:
+        get_prints()
