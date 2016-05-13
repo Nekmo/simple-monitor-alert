6eb0d29b7183297f2cf474548941558df8bacfc0
Nekmo <contacto@nekmo.com>
2016-05-13 04:31:57 +0200

    Fixed hddtemp key.

--- monitors/hddtemp.sh
+++ monitors/hddtemp.sh
@@ -6,8 +6,8 @@ IFS=$'\n'
 for disk in `lsblk -d -o name | tail -n +2`; do
     temp=`sudo hddtemp -n /dev/$disk 2> /dev/null`
     if [[ $temp =~ $re ]]; then
-        echo "smart_status($disk).name = 'HDD temperature result on /dev/$disk'"
-        echo "smart_status($disk).expected = <= 46"
-        echo "smart_status($disk).value = $temp"
+        echo "hdd_temp($disk).name = 'HDD temperature result on /dev/$disk'"
+        echo "hdd_temp($disk).expected = <= 46"
+        echo "hdd_temp($disk).value = $temp"
     fi
 done
