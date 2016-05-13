8c7856888753636a2e26331158422189e407cce5
Nekmo <contacto@nekmo.com>
2016-05-13 04:28:43 +0200

    Fixed hddtemp group.

--- monitors/hddtemp.sh
+++ monitors/hddtemp.sh
@@ -6,8 +6,8 @@ IFS=$'\n'
 for disk in `lsblk -d -o name | tail -n +2`; do
     temp=`sudo hddtemp -n /dev/$disk 2> /dev/null`
     if [[ $temp =~ $re ]]; then
-        echo "smart_status.name = 'HDD temperature result on /dev/$disk'"
-        echo "smart_status.expected = <= 46"
-        echo "smart_status.value = $temp"
+        echo "smart_status($disk).name = 'HDD temperature result on /dev/$disk'"
+        echo "smart_status($disk).expected = <= 46"
+        echo "smart_status($disk).value = $temp"
     fi
 done
