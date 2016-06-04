d598c11599ba3a4b3e4305ae31e297998ef8a8d6
Nekmo <contacto@nekmo.com>
2016-06-04 17:41:37 +0200

    Docker testing.

---
+++ docker/Debian7.10-sma/tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pip install git+https://github.com/Nekmo/simple-monitor-alert.git@v0.2.3#egg=simple-monitor-alert
+ls /etc/simple-monitor-alert/sma.ini
---
+++ docker/tests.py
@@ -0,0 +1,59 @@
+import os
+import subprocess
+
+import sys
+from colorclass import Color
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+
+
+def execute(cmds, output=False):
+    output = None if output else subprocess.PIPE
+    p = subprocess.Popen(cmds, stdout=output, stderr=output)
+    p.wait()
+    if p.returncode != 0:
+        print(Color('{red}ERROR!{/red}'))
+        if output is not None:
+            print('STDOUT: {}'.format(p.stdout))
+            print('STDERR: {}'.format(p.stderr))
+        raise ChildProcessError
+
+
+def test_image(name, image_output=False, container_output=False):
+    print_= print if image_output or container_output else sys.stdout.write
+    print_(Color('{yellow}TESTING:{/yellow} {magenta}{}{/magenta}... ').format(name))
+    dir = os.path.join(__dir__, name)
+    image_name = name.lower()
+    execute(['docker', 'build', '-t', image_name, dir], image_output)
+    execute(['docker', 'run', '--rm', image_name], container_output)
+    print(Color('{green}SUCCESS{/green}'))
+
+
+def test_all(image_output=False, container_output=False):
+    results = []
+    for dirname in os.listdir(__dir__):
+        if not os.path.isdir(dirname):
+            continue
+        try:
+            test_image(dirname, image_output=image_output, container_output=container_output)
+        except ChildProcessError:
+            results.append(False)
+        else:
+            results.append(True)
+    print('-' * 60)
+    print('PASSED: {} {}/{}'.format(
+        ''.join([{False: Color.red('F'), True: Color.green('.')}[x] for x in results]),
+        len(list(filter(lambda x: x, results))), len(results)
+    ))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Test images.')
+    parser.add_argument('image', nargs='?')
+    parser.add_argument('--image-output', action='store_true')
+    parser.add_argument('--container-output', action='store_true')
+    args = parser.parse_args()
+    if args.image:
+        test_image(args.image, args.image_output, args.container_output)
+    else:
+        test_all(args.image_output, args.container_output)
