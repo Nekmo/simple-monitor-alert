8012c2bce740c4373d7904e429fef0a418e39e08
Nekmo <nekmo@alsur.es>
2016-06-12 18:52:08 +0200

    Fabric compile simple-monitor-alert from AUR.

--- fabfile.py
+++ fabfile.py
@@ -10,10 +10,11 @@ except ImportError:
     deploy = None
 
 
+PACKAGE = 'simple-monitor-alert'
 env.hosts = getattr(deploy, 'hosts', [])
 remote_tmp_dir = os.path.join('/tmp', uuid.uuid4().hex)
 
-archlinux_aur_packages = ['python-humanize', 'python-colorclass-git', 'python-terminaltables']
+archlinux_aur_packages = ['python-humanize', 'python-colorclass-git', 'python-terminaltables', PACKAGE]
 
 
 def _remove_tmp_dir():
@@ -34,12 +35,25 @@ def _build_aur_package(package):
 
 
 def arch_repo():
+    run('mkdir -p {}'.format(remote_tmp_dir))
+    package_dir = os.path.join(remote_tmp_dir, 'git-'.format(PACKAGE))
+    run('mkdir -p {}'.format(package_dir))
+    with cd(package_dir):
+        run('git clone git+ssh://aur@aur.archlinux.org/{}.git .'.format(PACKAGE))
+        put('packages/archlinux/PKGBUILD', package_dir)
+        put('packages/archlinux/*.install', package_dir)
+        run('makepkg --printsrcinfo > .SRCINFO')
+        run('git add .SRCINFO')
+        run('git add PKGBUILD')
+        run('git add *.install')
+        with settings(warn_only=True):
+            result = run('git commit -m "Auto: Updated"')
+        if result.return_code == 0:
+            run('git push')
     for package in archlinux_aur_packages:
-        run('mkdir -p {}'.format(remote_tmp_dir))
         pkg = _build_aur_package(package)
         run('mv {} {}'.format(pkg, deploy.archlinux_repo_dir))
         run('mv {}.sig {}'.format(pkg, deploy.archlinux_repo_dir))
-        _remove_tmp_dir()
-
     with cd(deploy.archlinux_repo_dir):
         run('repo-add --sign repo.db.tar.gz *.pkg.tar.xz')
+    _remove_tmp_dir()
