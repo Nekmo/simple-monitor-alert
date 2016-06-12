5bf9542c57e038d89ed30f6bfac9b58fe9bbf99f
Nekmo <nekmo@alsur.es>
2016-06-12 15:38:37 +0200

    Fabric Arch deployment deps

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,7 @@ except ImportError:
 env.hosts = getattr(deploy, 'hosts', [])
 remote_tmp_dir = os.path.join('/tmp', uuid.uuid4().hex)
 
-archlinux_aur_packages = ['python-colorclass-git', 'python-humanize', 'python-terminaltables']
+archlinux_aur_packages = ['python-humanize', 'python-colorclass-git', 'python-terminaltables']
 
 
 def _remove_tmp_dir():
@@ -25,7 +25,8 @@ def _build_aur_package(package):
         run('git clone https://aur.archlinux.org/{}.git'.format(package))
     tmp_build_dir = os.path.join(remote_tmp_dir, package)
     with cd(tmp_build_dir):
-        run('makepkg')
+        run('gpg-connect-agent reloadagent /bye')
+        run('makepkg --sign')
         pkgs = run('ls -1 *.pkg.tar.xz').splitlines()
     if not pkgs:
         raise Exception('The package {} could not be compiled.'.format(package))
@@ -33,10 +34,12 @@ def _build_aur_package(package):
 
 
 def arch_repo():
-    run('mkdir -p {}'.format(remote_tmp_dir))
     for package in archlinux_aur_packages:
+        run('mkdir -p {}'.format(remote_tmp_dir))
         pkg = _build_aur_package(package)
         run('mv {} {}'.format(pkg, deploy.archlinux_repo_dir))
+        run('mv {}.sig {}'.format(pkg, deploy.archlinux_repo_dir))
+        _remove_tmp_dir()
+
     with cd(deploy.archlinux_repo_dir):
-        run('repo-add repo.db.tar.gz *.pkg.tar.xz')
-    _remove_tmp_dir()
+        run('repo-add --sign repo.db.tar.gz *.pkg.tar.xz')
